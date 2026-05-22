cfb46640540cc1e411d91b29634440a6c30e035d
Eldar <semenoveldar25@gmail.com>
2026-05-23 02:27:15 +0300

    Update CORS origin for MovieController

--- src/main/java/com/example/demo/controller/MovieController.java
+++ src/main/java/com/example/demo/controller/MovieController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
- @CrossOrigin(origins = "http://localhost:5173")
+@CrossOrigin(origins = "https://moviediary-frontend.vercel.app")
 @RestController
 @RequestMapping("/movie")
 public class MovieController {
